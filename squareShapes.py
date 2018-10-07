d631161f303ede960780fc5117b7498df3855afc
Rahul Yadav <rahulyadav1117@gmail.com>
2018-10-07 20:26:03 +0530

    create squrae shape

---
+++ squareShapes.py
@@ -0,0 +1,26 @@
+import turtle
+
+def draw_saqure():
+    window = turtle.Screen()
+    window.bgcolor('black')
+    
+    saq=turtle.Turtle()
+    saq.color('white')
+    saq.speed(3)
+
+
+    saq.forward(100)
+    saq.right(90)
+
+    saq.forward(100)
+    saq.right(90)
+
+    saq.forward(100)
+    saq.right(90)
+
+    saq.forward(100)
+    saq.right(90)
+    
+
+draw_saqure()
+    
